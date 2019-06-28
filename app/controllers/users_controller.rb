c9d276b307a8819f9ce631d802fd04ec910af14d
markgllin <markgllin@hotmail.com>
2019-06-27 18:30:43 -0600

    7.16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+  # this creates a form for the user object @ /users/new
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # this is called by POST-ing to /users (done by new.html.erb view)
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # handle successful save
+    else
+      render 'new'
+    end
   end
 end
+
+# https://www.railstutorial.org/book/sign_up#fig-signup_failure_rails_debug
